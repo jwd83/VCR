1ccb0b438d0a0b475a214e07ad51353e394ae840
Jared <jareddeblander@gmail.com>
2020-07-03 11:48:34 -0400

    single file release

--- index.php
+++ index.php
@@ -1,6 +1,573 @@
 <?php
 
-include dirname(__FILE__) . "vcrlib.php";
+# track runtime
+$start = microtime(true);
+$rustart = getrusage();
+
+//  /$$         /$$                   /$$
+// | $$        | $$                  | $$
+// | $$$$$$$  /$$$$$$   /$$$$$$/$$$$ | $$
+// | $$__  $$|_  $$_/  | $$_  $$_  $$| $$
+// | $$  \ $$  | $$    | $$ \ $$ \ $$| $$
+// | $$  | $$  | $$ /$$| $$ | $$ | $$| $$
+// | $$  | $$  |  $$$$/| $$ | $$ | $$| $$
+// |__/  |__/   \___/  |__/ |__/ |__/|__/
+                                      
+//  /$$                                    
+// | $$                                    
+// | $$$$$$$   /$$$$$$   /$$$$$$$  /$$$$$$ 
+// | $$__  $$ |____  $$ /$$_____/ /$$__  $$
+// | $$  \ $$  /$$$$$$$|  $$$$$$ | $$$$$$$$
+// | $$  | $$ /$$__  $$ \____  $$| $$_____/
+// | $$$$$$$/|  $$$$$$$ /$$$$$$$/|  $$$$$$$
+// |_______/  \_______/|_______/  \_______/
+                                        
+
+define('THEME_TOP' , <<<EOL
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Highwind's Stash</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/full-width-pics.css" rel="stylesheet">
+
+  <!-- https://stackoverflow.com/questions/9789723/css-text-overflow-in-a-table-cell -->
+  
+  <style>
+
+.text-overflow-dynamic-container {
+    position: relative;
+    max-width: 100%;
+    padding: 0 !important;
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: flex;
+    vertical-align: text-bottom !important;
+}
+.text-overflow-dynamic-ellipsis {
+    position: absolute;
+    white-space: nowrap;
+    overflow-y: visible;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+    -ms-text-overflow: ellipsis;
+    -o-text-overflow: ellipsis;
+    max-width: 100%;
+    min-width: 0;
+    width:100%;
+    top: 0;
+    left: 0;
+}
+.text-overflow-dynamic-container:after,
+.text-overflow-dynamic-ellipsis:after {
+    content: '-';
+    display: inline;
+    visibility: hidden;
+    width: 0;
+}
+
+  </style>
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="/gd/">Highwind's Stash</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/gd/">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?c=a">Audio Books</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?c=b">Books</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?c=e">Emulation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?c=m">Music</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./?c=v">Movies+TV</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+EOL
+);
+
+define('THEME_BOTTOM', <<<EOL
+
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <a href="https://github.com/jwd83/VCR"><p class="m-0 text-center text-white" style="
+      -moz-transform: rotate(-180deg);
+    -ms-transform: rotate(-180deg);
+    -o-transform: rotate(-180deg);
+    -webkit-transform: rotate(-180deg);
+    transform: rotate(-180deg);
+
+    ">Copyleft &copy;</p></a>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
+
+
+
+
+EOL );
+
+//   /$$$$$$                                 /$$     /$$                              
+//  /$$__  $$                               | $$    |__/                              
+// | $$  \__//$$   /$$ /$$$$$$$   /$$$$$$$ /$$$$$$   /$$  /$$$$$$  /$$$$$$$   /$$$$$$$
+// | $$$$   | $$  | $$| $$__  $$ /$$_____/|_  $$_/  | $$ /$$__  $$| $$__  $$ /$$_____/
+// | $$_/   | $$  | $$| $$  \ $$| $$        | $$    | $$| $$  \ $$| $$  \ $$|  $$$$$$ 
+// | $$     | $$  | $$| $$  | $$| $$        | $$ /$$| $$| $$  | $$| $$  | $$ \____  $$
+// | $$     |  $$$$$$/| $$  | $$|  $$$$$$$  |  $$$$/| $$|  $$$$$$/| $$  | $$ /$$$$$$$/
+// |__/      \______/ |__/  |__/ \_______/   \___/  |__/ \______/ |__/  |__/|_______/ 
+
+# settings
+
+// $path_list_html = "list.html";
+$extensions = [
+    ".avi", 
+    ".flac",
+    ".m4b", // uncommon audio format
+    ".mkv", 
+    ".mp3", 
+    ".mp4", 
+    ".ogg", 
+    ".iso", 
+    ".smc",
+    ".wma",
+
+    ".whitelistme"
+];
+$white_list = [
+    "Emulation + ROMs\\" ,
+    "D&D\\", 
+    "Books\\"
+];
+$excludes = ["Backups+Temp", "Software+Utilities", "Dropbox"];
+
+function drawHeader($banner = false) 
+{
+    // global 
+    echo THEME_TOP;
+    if ($banner !== false) 
+    {
+        ?>
+<!-- Header - set the background image for the header in the line below -->
+<header class="py-5 bg-image-full" style="background-image: url('/gd/black-pearl.jpg');">
+<div class="container">
+<h1 style="
+    color:#fff;
+    margin-bottom: 30px;
+    margin-top: 30px;
+    font-size: 5em;
+    opacity: 0.7;
+    -webkit-text-stroke: 3px black;
+    font-style: italic;
+    font-family: serif;
+    font-weight: bolder;
+
+    ">
+
+    &ldquo;<?= $banner ?>&rdquo;
+</h1>
+</div>
+<!-- <img class="img-fluid d-block mx-auto" src="/gd/pirate-symbol.jpg" alt="" style="opacity: 0"> -->
+</header>
+
+        <?php
+    }
+    
+    echo 
+    '
+    <!-- Content section -->
+    <section class="py-5">
+    <div class="container">
+    ';
+}
+
+
+function strToHex($string){
+    $hex='';
+    for ($i=0;
+        $i < strlen($string); $i++){
+        $hex .= dechex(ord($string[$i]));
+    }
+    return $hex;
+}
+
+function hexToStr($hex){
+    $string='';
+    for ($i=0; $i < strlen($hex)-1; $i+=2){
+        $string .= chr(hexdec($hex[$i].$hex[$i+1]));
+    }
+    return $string;
+}
+
+function endsWith($haystack, $needle)
+{
+    $length = strlen($needle);
+ 
+    if ($length == 0) {
+        return true;
+    }
+
+    return (substr($haystack, -$length) === $needle);
+}
+
+function startsWith ($string, $startString) 
+{ 
+    $len = strlen($startString); 
+    return (substr($string, 0, $len) === $startString); 
+} 
+
+function getDirContents($dir, &$results = array()) {
+    $files = scandir($dir);
+
+    foreach ($files as $key => $value) 
+    {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+
+        if (!is_dir($path)) 
+        {
+            $results[] = $path;
+        } 
+        else if ($value != "." && $value != "..") 
+        {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+
+    return $results;
+}
+
+function echoCell($str) {
+//     echo '
+
+// <td>
+//   <span class="text-overflow-dynamic-container">
+//     <span class="text-overflow-dynamic-ellipsis" title="'.htmlentities($str).'">
+//       '.$str.'
+//     </span>
+//   </span>
+
+// </td>
+// ';   
+ echo '
+
+<td>
+      '.$str.'
+
+</td>
+';
+}
+
+function getButtons($base_path, $current_file) {
+
+    $hit = 0;
+    $hit_checks = 0;
+    $prev_file = "";
+    $button_array = array();
+
+    $button_array['previous'] = "";
+    $button_array['next'] = "";
+
+    $files = getDirContents($base_path);
+
+    foreach($files as $file) {
+        if(!is_dir($file)) {
+            $path = substr(trim($file), 3);
+            if($hit == 1) {
+                $button_array['previous'] = strToHex($prev_file);
+                $button_array['next'] = strToHex($path);
+
+                $button_array['previous_raw'] = $prev_file;
+                $button_array['next_raw'] = str_replace('\\', '/', $path);
+                return $button_array;
+
+            }
+            else
+            {
+                if(strToHex($path) == $current_file) {
+                    $hit = 1;
+                } else {
+                    $prev_file = $path;
+                }
+            }            
+        }
+
+    }
+
+    return $button_array;
+}
+
+
+function dumpPath($base_path, $optional_feature = "none", $optional_reference = "none", $query = "none") {
+    global $extensions, $white_list, $excludes;
+
+    echo "<h1>$base_path</h1>\n<table>\n";
+
+    $base_path = dirname(__FILE__) . $base_path;
+
+    
+    # generate list of files into this array
+
+    $files = getDirContents($base_path);
+    
+    # setup variables to filter our list for our static html cache page
+
+    $matched = 0;
+    $excluded = 0;
+    $skipped = 0;
+    $total = 0;
+
+    foreach($files as $file) 
+    {
+        $total++;
+        $valid = 0;
+
+        if(!is_dir($file)) {
+
+            $path = substr(trim($file), 3);
+
+            // check for a valid file extension
+
+            foreach($extensions as $ext) 
+            {
+                if (endsWith(strtolower($path), strtolower($ext))) 
+                {
+                    $valid = 1;
+                }
+            }
+
+            // check if the start of a path matches the whitelist
+            foreach($white_list as $wl) 
+            {
+                if (startsWith(strtolower($path), strtolower($wl))) 
+                {
+                    $valid = 1;
+                }
+            }
+
+            // if there is a query set forget everything we just did and check against the query
+            if($query != 'none') {
+                $valid = 0;
+                if (strpos(strtolower($path), strtolower($query)) !== false) {
+                    $valid = 1;
+                }
+            }
+
+            // check that it's not in an excluded path
+            foreach($excludes as $exclude) 
+            {
+                if (strpos(strtolower($path), strtolower($exclude)) !== false) {
+                    $excluded++;
+                    if($valid == 1) {
+                        $matched--;
+                        $skipped++;
+
+                    }
+                    $valid = 0;
+
+                }
+            }
+        }
+
+
+        if($valid === 1) 
+        {
+            $matched++;
+
+            echo "<tr>\n";
+            echoCell('<a href="'.$path.'">[direct]</a>');
+            if($optional_feature != 'none') {
+                echoCell('<a href="?c='.$optional_reference.'&file='.strToHex($path).'">['.$optional_feature.']</a>');
+            }
+            echoCell(substr($path,  strlen($base_path)-2));
+            echo "</tr>\n";
+        }
+    }
+    echo "</table><hr><div>$matched matched of $total files searched, $skipped of $excluded excluded</div>";
+}
+
+function refreshList() {
+
+    global $extensions, $white_list, $excludes;
+
+    if(isset($_REQUEST["refresh"])) 
+    {
+        # generate list of files into this array
+        $files = getDirContents(dirname(__FILE__));
+        
+
+        # write this array to list.txt
+        file_put_contents('list.txt', implode(PHP_EOL, $files));
+
+
+        # setup variables to filter our list for our static html cache page
+
+        $matched = 0;
+        $excluded = 0;
+        $skipped = 0;
+        $total = 0;
+
+        # create a static html cache
+        $my_file = fopen($path_list_html, "w");
+
+
+
+        foreach($files as $file) 
+        {
+            $total++;
+            $valid = 0;
+
+            $path = substr(trim($file), 3);
+
+            // check for a valid file extension
+
+            foreach($extensions as $ext) 
+            {
+                if (endsWith(strtolower($path), strtolower($ext))) 
+                {
+                    $matched++;
+                    $valid = 1;
+                }
+            }
+
+            // check if it's in a whitelisted path
+
+
+            foreach($white_list as $wl) 
+            {
+                if (strpos(strtolower($path), strtolower($wl)) !== false) {
+                    $matched++;
+                    $valid = 1;
+                }
+            }
+
+
+            // check that it's not in an excluded path
+
+
+            foreach($excludes as $exclude) 
+            {
+                if (strpos(strtolower($path), strtolower($exclude)) !== false) {
+                    $excluded++;
+                    if($valid == 1) {
+                        $matched--;
+                        $skipped++;
+
+                    }
+                    $valid = 0;
+
+                }
+            }
+
+
+            if($valid === 1) 
+            {
+                fwrite($my_file, '
+    <div>
+        <a href="'.$path.'">[d]</a>
+        <a href="v.php?file='.$path.'">[v]</a>
+        <a href="a.php?file='.$path.'">[a]</a>
+        <a href="'.$path.'">'.$path.'</a>
+    </div>
+    ');
+            }
+        }
+
+        fwrite($my_file, "<hr><div>$matched matched of $total files searched, $skipped of $excluded excluded</div>");
+
+        fclose($my_file);
+    }
+}
+
+
+function rutime($ru, $rus, $index) {
+    return ($ru["ru_$index.tv_sec"]*1000 + intval($ru["ru_$index.tv_usec"]/1000))
+     -  ($rus["ru_$index.tv_sec"]*1000 + intval($rus["ru_$index.tv_usec"]/1000));
+}
+
+
+
+function drawFooter() {
+    global $start, $rustart;
+
+    echo "<hr>";
+    $ru = getrusage();
+    $time_elapsed_secs = microtime(true) - $start;
+
+
+
+    echo "This process used " . rutime($ru, $rustart, "utime") . " ms for its computations.<br>\n";
+    echo "It spent " . rutime($ru, $rustart, "stime") . " ms in system calls.<br>\n";
+    echo '<b>Total Execution Time:</b> '.$time_elapsed_secs.' seconds';
+
+    echo THEME_BOTTOM;
+
+
+}
+
+function buildPlaybackLink($file_in_hex, $type = "") {
+
+    # determine type
+    if(strlen($type) == 0) {
+        if(isset($_REQUEST['c'])) {
+            $type = $_REQUEST['c'];
+        }
+
+    }
+
+    # filename must already be in hex format
+    return "/gd/?c=" . $type . "&file=".$file_in_hex;
+}
+
+$suggestions["m"] = ["daft", "radiohead","Eilish" ];
 
 $search_type = '';
 $dump_path = '';
@@ -11,11 +578,22 @@ $buttons = array();
 $next_file_to_play = "";
 
 
+// showSuggestions($category ){
+//     $sugs = $GLOBALS['suggestions'];
+//     foreach($sugs[$category] as $suggestion) {
+        
+//     }
+// }
+
+
+                                                  
+
 
 if(!isset($_REQUEST['c'])) {
     drawHeader("Stay Awhile and Listen");
 
     echo "<h1>Please enjoy your stay and DM requests</h1>";
+
 } else {
     # config switch
     switch($_REQUEST['c']){
@@ -144,6 +722,8 @@ if(isset($_REQUEST['file']) && isset($_REQUEST['c'])) {
 
 // autoplay binding
 
+
+
 $(\"#player\").bind(
     'ended', 
     function() {
@@ -166,9 +746,8 @@ setTimeout(
     }, 
     10000
 );
-
-
             ';
+
         }
     }
 }
