bd7f53ce946cf77966677af1f06e3a9d4fd21ba4
Jared <jareddeblander@gmail.com>
2020-08-27 13:54:05 -0400

    started single file python daemon, updated ascii art links

---
+++ daemon.py
@@ -0,0 +1,81 @@
+# Python daemon for VCR
+#
+# Distributed under the
+#
+# MIT License
+#
+# Copyright (c) 2020 Jared
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ASCII ART FROM
+# http://patorjk.com/software/taag/#p=display&c=bash&f=Big%20Money-ne&t=encoding
+#
+#
+
+import json
+import mariadb
+import os
+import sys
+import time
+
+def new_extension(src, new_ext):
+    filename, file_extension = os.path.splitext(src)
+    return filename + new_ext
+
+#                                                 /$$ /$$
+#                                                | $$|__/
+#    /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$$ /$$ /$$$$$$$   /$$$$$$
+#   /$$__  $$| $$__  $$ /$$_____/ /$$__  $$ /$$__  $$| $$| $$__  $$ /$$__  $$
+#  | $$$$$$$$| $$  \ $$| $$      | $$  \ $$| $$  | $$| $$| $$  \ $$| $$  \ $$
+#  | $$_____/| $$  | $$| $$      | $$  | $$| $$  | $$| $$| $$  | $$| $$  | $$
+#  |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$/|  $$$$$$$| $$| $$  | $$|  $$$$$$$
+#   \_______/|__/  |__/ \_______/ \______/  \_______/|__/|__/  |__/ \____  $$
+#                                                                   /$$  \ $$
+#                                                                  |  $$$$$$/
+#                                                                   \______/
+
+def encode_h265(src):
+    out = new_extension(src, ".h265.mp4")
+
+def encode_h264(src):
+    out = new_extension(src, ".h264.mp4")
+
+def encode_opus(src):
+    out = new_extension(src, ".opus")
+
+#                           /$$
+#                          |__/
+#   /$$$$$$/$$$$   /$$$$$$  /$$ /$$$$$$$
+#  | $$_  $$_  $$ |____  $$| $$| $$__  $$
+#  | $$ \ $$ \ $$  /$$$$$$$| $$| $$  \ $$
+#  | $$ | $$ | $$ /$$__  $$| $$| $$  | $$
+#  | $$ | $$ | $$|  $$$$$$$| $$| $$  | $$
+#  |__/ |__/ |__/ \_______/|__/|__/  |__/
+#
+#
+#
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
--- index.php
+++ index.php
@@ -1,22 +1,41 @@
 <?php
-//
-// shout outs to all my homies in VA
-//
-// ascii art from patorjk.com
-//
-// http://patorjk.com/software/taag/#p=display&c=c%2B%2B&f=Big%20Money-ne&t=begins
-//
-//                        /$$     /$$     /$$
-//                       | $$    | $$    |__/
-//   /$$$$$$$  /$$$$$$  /$$$$$$ /$$$$$$   /$$ /$$$$$$$   /$$$$$$   /$$$$$$$
-//  /$$_____/ /$$__  $$|_  $$_/|_  $$_/  | $$| $$__  $$ /$$__  $$ /$$_____/
-// |  $$$$$$ | $$$$$$$$  | $$    | $$    | $$| $$  \ $$| $$  \ $$|  $$$$$$
-//  \____  $$| $$_____/  | $$ /$$| $$ /$$| $$| $$  | $$| $$  | $$ \____  $$
-//  /$$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$/| $$| $$  | $$|  $$$$$$$ /$$$$$$$/
-// |_______/  \_______/   \___/   \___/  |__/|__/  |__/ \____  $$|_______/
-//                                                      /$$  \ $$
-//                                                     |  $$$$$$/
-//                                                      \______/
+# MIT License
+#
+# Copyright (c) 2020 Jared
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ASCII ART FROM
+# http://patorjk.com/software/taag/#p=display&c=bash&f=Big%20Money-ne&t=encoding
+#
+#                         /$$     /$$     /$$
+#                        | $$    | $$    |__/
+#    /$$$$$$$  /$$$$$$  /$$$$$$ /$$$$$$   /$$ /$$$$$$$   /$$$$$$   /$$$$$$$
+#   /$$_____/ /$$__  $$|_  $$_/|_  $$_/  | $$| $$__  $$ /$$__  $$ /$$_____/
+#  |  $$$$$$ | $$$$$$$$  | $$    | $$    | $$| $$  \ $$| $$  \ $$|  $$$$$$
+#   \____  $$| $$_____/  | $$ /$$| $$ /$$| $$| $$  | $$| $$  | $$ \____  $$
+#   /$$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$/| $$| $$  | $$|  $$$$$$$ /$$$$$$$/
+#  |_______/  \_______/   \___/   \___/  |__/|__/  |__/ \____  $$|_______/
+#                                                       /$$  \ $$
+#                                                      |  $$$$$$/
+#                                                       \______/
+
 # track runtime
 $start = microtime(true);
 $rustart = getrusage();
